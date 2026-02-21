6c4fc0d883670053c92f793a5845e223fd73c9b5
EmersonBJ <ewbranco2004@gmail.com>
2026-02-21 18:00:49 -0300

    update Q

--- Q1.py
+++ Q1.py
@@ -1,12 +1,23 @@
+#Question 1.
+# ==============================================================================
+# Mostre qual é o maior e o menor número flutuante que seu computador trabalha,
+# realmax e realmin no MatLab.
+# Mostre qual é o epsilon de sua máquina, eps no MatLab. O que quer dizer cada um destes valores?
+#
+# Avaliar o erro da expressão:
+# f(x) = ((1 + x) - 1) / x
+# Para x = {1.e-15, 1.e+15}. Calcule os erros absolutos e relativos. Discuta a diferença nos resultados.
+
+
 import math
 import numpy as np
 
 #para usar o comando realmax no python, podemos usar o seguinte código:
-def realmax():return np.finfo(np.float64).max
-def realmin():return np.finfo(np.float64).tiny
-def epsilon():return np.finfo(np.float64).eps
-
-print(f"Realmax: {realmax()}; Realmin: {realmin()}; Epsilon: {epsilon()}")
+def realmax():return np.finfo(np.float64).max #retorna o maior número representável em ponto flutuante de 64 bits (float64), esperamos algo próximo de 2^1023 * (2 - 2^(-52)) ~ 1.7976931348623157e+308
+def realmin():return np.finfo(np.float64).tiny #retorna o menor número positivo representável em ponto flutuante de 64 bits (float64), esperamos algo próximo de 2^(-1022) ~ 10^(-308) ~ 2.2250738585072014e-308
+def epsilon():return np.finfo(np.float64).eps #retorna a menor diferença positiva entre 1.0 e o próximo número representável em ponto flutuante de 64 bits (float64), esperamos algo próximo de 2^(-52) ~ 10^(-16) ~ 2.220446049250313e-16
+#Ja que 64 bits (52 bits de precisão + 11 bits de expoente + 1 bit de sinal[IEEE 754]) tem uma precisão de aproximadamente 15-17 dígitos decimais.
+# 2^52 = 10^15.95[eps], 2^11 = 2048, dividido em: 2^(-1022) ~ 10^(-308) a 2^(1023) ~ 10^(308)
 
 def f(x):
     return (1 + x - 1)/x if x != 0 else float('inf')
--- Q5.py
+++ Q5.py
@@ -1,16 +1,14 @@
-# Um Problema Real: O Pêndulo e a Busca de Ângulos
+# ==============================================================================
+# Question 5. Um Problema Real: O Pêndulo e a Busca de Ângulos
+# ==============================================================================
 # Objetivo: Usar métodos numéricos para resolver um problema físico real.
+#
 # Descrição da Tarefa:
-# 
-# 1. Considere a equação transcendental do período aproximado de um pêndulo simples para grandes am-
-# plitudes:
-# 
-# f(θ) = sin(θ) − (T / 2π) * √(g / L) = 0
-# 
-# onde T é o período medido, L o comprimento e g a gravidade.
-# 2. Dado um período T observado, estime o ângulo inicial θ.
+# 1. Considere a equação transcendental do período aproximado de um pêndulo simples:
+#    f(theta) = math.sin(theta) - (T / (2 * math.pi)) * math.sqrt(g / L) = 0
+#    onde T é o período medido, L o comprimento e g a gravidade.
+# 2. Dado um período T observado, estime o ângulo inicial theta.
 # 3. Resolva o problema usando: bisseção, Newton-Raphson e secante.
 # 4. Analise o efeito de aproximações numéricas (arredondamento) no resultado.
-# 
-# Entrega: Código em Python ou outra linguagem, gráficos mostrando a evolução das aproximações e dis-
-# cussão da aplicabilidade do modelo.
+#
+# Entrega: Código, gráficos mostrando a evolução das aproximações e discussão do modelo.
