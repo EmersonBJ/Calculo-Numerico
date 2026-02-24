aa4785e18db6142cba3c1a19409c36bdd99da8b6
Emerson B.J. <141978191+EmersonBJ@users.noreply.github.com>
2026-02-24 18:45:07 -0300

    Refactor error evaluation in MATLAB script

--- MATLAB/Q1.m
+++ MATLAB/Q1.m
@@ -1,29 +1,19 @@
-clear; clc;
-
-% 1. Mostrando os Limites da Máquina
-fprintf('--- LIMITES DA MÁQUINA (MATLAB) ---\n');
 fprintf('Maior número flutuante (realmax): %e\n', realmax);
 fprintf('Menor número flutuante (realmin): %e\n', realmin);
-fprintf('Epsilon da máquina (eps):         %e\n\n', eps);
+fprintf('Epsilon da máquina (eps): %e\n', eps);
 
-% 2. Avaliação de Erros da Expressão f(x) = ((1 + x) - 1) / x
-v_exato = 1.0; % Matematicamente, o resultado deveria ser sempre 1
-x_vals = [1e-15, 1e15];
+v= 1.0;
+a = [1e-15, 1e15];
 
-fprintf('--- AVALIAÇÃO DOS ERROS ---\n');
-for i = 1:length(x_vals)
-    x = x_vals(i);
-    
-    % Cálculo da função
+for i = 1:length(a)
+    x = a(i);
+   
     f_x = ((1.0 + x) - 1.0) / x;
     
-    % Cálculo dos erros
-    erro_abs = abs(v_exato - f_x);
-    erro_rel = erro_abs / abs(v_exato);
+    erro_absoluto = abs(v - f_x);
+    erro_relativo = erro_absoluto / abs(v);
     
-    % Exibição
-    fprintf('Para x = %g:\n', x);
-    fprintf('  Resultado calculado : %1.15f\n', f_x);
-    fprintf('  Erro Absoluto       : %e\n', erro_abs);
-    fprintf('  Erro Relativo       : %e\n\n', erro_rel);
+    fprintf('  Valor calculado: %1.15f\n', f_x);
+    fprintf('  Erro Absoluto:   %e\n', erro_absoluto);
+    fprintf('  Erro Relativo:   %e\n\n', erro_relativo);
 end
